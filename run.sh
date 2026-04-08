87c8039ce8ab7e7b3016e5f2c8c357245889d00f
ROKMK <kmkrakowwroznychodslonach@gmail.com>
2026-04-08 17:40:00 +0200

    Add execute permission to update_sip.sh
    Make update_sip.sh executable before running it.

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ sleep 5
 # Przechodzimy do folderu apki
 cd /opt/krakow_sip_system
 
+chmod +x /opt/krakow_sip_system/update_sip.sh
+
 # Aktualizujemy i odpalamy
 ./update_sip.sh && python3 system.py
