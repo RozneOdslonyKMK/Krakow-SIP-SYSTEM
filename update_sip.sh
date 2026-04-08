3f0227c8a5c43fad933805890a5a736e81693268
ROKMK <kmkrakowwroznychodslonach@gmail.com>
2026-04-08 18:12:33 +0200

    Update update_sip.sh

--- update_sip.sh
+++ update_sip.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd /opt/krakow_sip_system
-
-# Dodaj to, aby Git nie marudził o uprawnienia:
 git config --global --add safe.directory /opt/krakow_sip_system
-
-# Reszta bez zmian
 git fetch --all
 git reset --hard origin/main
