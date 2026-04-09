2bf5470f8162adffed8a20b2deef69ff81972bd8
ROKMK <kmkrakowwroznychodslonach@gmail.com>
2026-04-09 19:15:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 sleep 10
 cd /opt/krakow_sip_system || exit
 /usr/bin/chmod +x ./update_sip.sh
-/usr/bin/bash ./update_sip.sh && /usr/bin/python3 system.py
+/usr/bin/bash ./update_sip.sh && /usr/bin/python3 system-linux.py
