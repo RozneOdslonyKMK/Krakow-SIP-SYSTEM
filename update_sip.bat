2d1fffefad6f0f29d685ab455a7a1620a3d0a36e
ROKMK <kmkrakowwroznychodslonach@gmail.com>
2026-04-16 01:30:09 +0200

    Moved

--- update_sip.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo [UPDATE] Sprawdzanie aktualizacji w repozytorium git...
-git config --global --add safe.directory "%cd:\=/%"
-git fetch --all
-git reset --hard origin/main
-echo [UPDATE] Gotowe.
--- update_sip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /opt/krakow_sip_system
-git config --global --add safe.directory /opt/krakow_sip_system
-git fetch --all
-git reset --hard origin/main
