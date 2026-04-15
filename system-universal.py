a06782a46d7eff74368fa049c749510bd24d8439
ROKMK <kmkrakowwroznychodslonach@gmail.com>
2026-04-15 14:04:19 +0200

    Główny kod aplikacji (uniwersalny)

--- system-universal.py
+++ system-universal.py
@@ -8,63 +8,6 @@ from system.top_screen import MainSIPLayout
 from system.vehicle_type import TypeSelectScreen
 from system.work_mode import StartModeScreen
 
-GPS_AVAILABLE = False
-try:
-    from gps import *
-    GPS_AVAILABLE = True
-except ImportError:
-    print("Biblioteka GPS nie znaleziona. Tryb automatyczny (Pojazd) będzie nieaktywny.")
-
-Config.set('kivy', 'keyboard_mode', 'systemanddock')
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-Window.fullscreen = 'auto'
-Window.show_cursor = False 
-
-now = datetime.now()
-formatted_news = f"******* Jakość powietrza: DOBRA. Godzina pomiaru: {now.strftime('%H')}:00 {now.strftime('%d-%m-%Y')}. *******"
-
-OPERATORS = {
-    "MPK Kraków": ["Tramwaj", "Autobus"],
-    "Mobilis": ["Autobus"],
-    "KLM": ["Tramwaj", "Autobus"]
-}
-
-SPECIAL_MODES = {
-    "TRAM_WYJAZD": {"line": "", "label": "Wyjazd na linię", "stops": True},
-    "TRAM_ZJAZD": {"line": "", "label": "Zajezdnia", "stops": True},
-    "BUS_WYJAZD": {"line": "", "label": "Wyjazd na linię", "stops": False},
-    "BUS_ZJAZD": {"line": "", "label": "Zjazd do zajezdni", "stops": False},
-    "PRZEJAZD_TECH": {"line": "", "label": "Przejazd Techniczny", "stops": False},
-    "NAUKA_JAZDY": {"line": "", "label": "Nauka Jazdy", "stops": False},
-    "JAZDA_TEST": {"line": "TEST", "label": "Jazda Testowa", "stops": False},
-    "JAZDA_PROBNA": {"line": "", "label": "Jazda Próbna", "stops": False},
-    "MPK_KRAKOW": {"line": "", "label": "MPK S.A. w Krakowie", "stops": False},
-    "MOBILIS": {"line": "", "label": "MOBILIS KRAKÓW", "stops": False}
-}
-
-VOICE_TYPES = {
-    "Stare": "audio",
-    "Nowe": "audio/new",
-    "Makłowicz": "audio/maklowicz"
-}
-
-SEARCH_ORDER = {
-    "audio/maklowicz": ["audio/maklowicz", "audio/new", "audio"],
-    "audio/new": ["audio/new", "audio", "audio/maklowicz"],
-    "audio": ["audio", "audio/new", "audio/maklowicz"]
-}
-
-SESSION = {
-    "mode": "Dom",
-    "operator": "",
-    "type": "",
-    "voice_path": "audio",
-    "news_text": formatted_news,
-    "is_route_changed": False,
-    "selected_csv_path": "",
-    "special_mode_id": None
-}
-
 class SipScreen(Screen):
     def setup_sip(self, csv_file):
         self.clear_widgets()
