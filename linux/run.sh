4b35ec068be316001770f020238fe516b8b7f231
ROKMK <kmkrakowwroznychodslonach@gmail.com>
2026-04-19 02:24:15 +0200

    Plik uruchamiający program

--- linux/run.sh
+++ linux/run.sh
@@ -47,6 +47,7 @@ echo "--- Zależności sprawdzone. Uruchamianie SIP ---"
 
 /usr/bin/chmod +x $RUN_DIR/update_sip.sh
 /usr/bin/bash $RUN_DIR/update_sip.sh
-/usr/bin/python3 $APP_DIR/system-universal.py
+/usr/bin/python3 $APP_DIR/system-universal.py sip
+/usr/bin/python3 $APP_DIR/system-universal.py driver
 
 echo "--- ZAMKNIĘCIE SYSTEMU: $(date +"%Y-%m-%d %H:%M:%S") ---"
